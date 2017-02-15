518498c47bcffb85a1a09855f77bd8381468c1f7
Shubham kanodia <shubham.kanodia@urbanladder.com>
2017-02-15 13:25:15 +0530

    Initial commit

---
+++ test/validate-config.js
@@ -0,0 +1,21 @@
+const eslint = require('eslint')
+const test = require('tape')
+const tapSpec = require('tap-spec');
+
+test.createStream()
+  .pipe(tapSpec())
+  .pipe(process.stdout)
+
+test('load config in eslint to validate all rule syntax is correct', (t) => {
+  const CLIEngine = eslint.CLIEngine
+
+  const cli = new CLIEngine({
+    useEslintrc: false,
+    configFile: 'eslintrc.js',
+  })
+
+  const code = 'const foo = 1\nconst bar = function() {}\nbar(foo)\n'
+
+  t.equal(cli.executeOnText(code).errorCount, 0)
+  t.end()
+})
